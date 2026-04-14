23fc971b603ef619c0515c250677b0f49e5bd4d6
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-14 18:04:15 -0500

    master

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,62 +1,58 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import Library from '../assets/Library.svg';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import Library from "../assets/Library.svg";
 
 const Nav = () => {
   return (
     <nav>
       <div className="nav__container">
+        
         <a href="/">
           <img src={Library} alt="Library logo" className="logo" />
         </a>
 
         <ul className="nav__links">
           <li className="nav__list">
-            <a href="/" className="nav__link">
-              Home
-            </a>
+            <a href="/" className="nav__link">Home</a>
           </li>
 
           <li className="nav__list">
-            <a href="/" className="nav__link">
-              Books
-            </a>
+            <a href="/books" className="nav__link">Books</a>
           </li>
+
+          <li className="nav__icon">
+            <a href="/cart" className="nav__link">
+              <FontAwesomeIcon icon={faShoppingCart} />
+            </a>
+            <span className="cart__length">2</span>
+          </li>
+        </ul>
 
         <button className="btn__menu">
           <FontAwesomeIcon icon={faBars} />
         </button>
 
-        <li className="nav__icon">
-          <a href="/cart" className="nav__link">
-            <FontAwesomeIcon icon="shopping-cart" />
-          </a>
-          <span className="cart__length">2</span>
-        </li>
-        </ul>
         <div className="menu__backdrop">
-            <button className="btn__menu btn__menu--close">
-                <FontAwesomeIcon icon="times" />
-            </button>
-            <ul className="menu__links">
-                <li className="menu__list">
-                    <a href="/" className="menu__link">
-                    Home
-                    </a>
-                </li>
+          <button className="btn__menu btn__menu--close">
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+
+          <ul className="menu__links">
             <li className="menu__list">
-                    <a href="/books" className="menu__link">
-                    Books
-                    </a>
-                </li>
+              <a href="/" className="menu__link">Home</a>
+            </li>
+
             <li className="menu__list">
-                    <a href="/cart" className="menu__link">
-                    Cart
-                    </a>
-                </li>
-            </ul>
+              <a href="/books" className="menu__link">Books</a>
+            </li>
+
+            <li className="menu__list">
+              <a href="/cart" className="menu__link">Cart</a>
+            </li>
+          </ul>
         </div>
+
       </div>
     </nav>
   );
